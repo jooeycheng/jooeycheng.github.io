3a7d8379ee68968cde82488dd791d082533ccc1f
Joey Cheng <jooeycheng@gmail.com>
2016-05-11 20:20:49 +0800

    add adele hello easter egg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,30 @@
 $( document ).ready(function() {
   smoothScroll.init();
   $("#email").attr("href", "mailto:hello@jooeycheng.me");
+
+  var helloAudio = new Audio('/assets/hello.mp3');
+
+  $(document).on({
+    mouseenter: function() {
+      // $("#spinner").addClass("show");
+      $(".hello-container").addClass("adele");
+      helloAudio.play();
+    },
+    mouseleave: function() {
+      // $("#spinner").removeClass("show");
+      $(".hello-container").removeClass("adele");
+      helloAudio.pause();
+      helloAudio.currentTime = 0;
+    }
+  }, ".hello-text");
+
+  $(document).on('mousemove', function(e) {
+    $('#spinner').css({
+      left: e.pageX + 15,
+      top:  e.pageY
+    });
+  });
+
 });
 
 $( window ).load(function() {
@@ -8,5 +32,5 @@ $( window ).load(function() {
 });
 
 particlesJS.load('particles', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+  // console.log('callback - particles.js config loaded');
 });
